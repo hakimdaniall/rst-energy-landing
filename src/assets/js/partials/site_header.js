be119a1c8b788053013f572f829b86d85e0c24c0
hakim@auronex.com <dev.hakimdanial@gmail.com>
2024-11-02 01:37:10 +0800

    fix site header selected

--- src/assets/js/partials/site_header.js
+++ src/assets/js/partials/site_header.js
@@ -1,13 +1,18 @@
 (function () {
-    document.addEventListener("DOMContentLoaded", function() {
-      const currentUrl = window.location.pathname.split("/").pop();
+  document.addEventListener("DOMContentLoaded", function() {
+      // If on the root path, set currentUrl to "index", else remove leading/trailing slashes
+      const currentUrl = window.location.pathname === "/" ? "index" : window.location.pathname.replace(/^\/|\/$/g, "");
+
       const menuLinks = document.querySelectorAll("#navmenu a");
-    
+
       menuLinks.forEach(link => {
-        if (link.getAttribute("href") === currentUrl) {
-          link.classList.add("active");
-        }
+          // Normalize href by removing leading slash
+          const linkHref = link.getAttribute("href").replace(/^\/|\/$/g, "") || "index";
+
+          // Add active class if href matches currentUrl
+          if (linkHref === currentUrl) {
+              link.classList.add("active");
+          }
       });
-    });
+  });
 })();
-  
